f563f7513a09697c0540695f74ff07af77d00696
b0989596914 <92772248+b0989596914@users.noreply.github.com>
2021-12-10 22:08:24 +0800

    Create react.py
    add

---
+++ cmds/react.py
@@ -0,0 +1,25 @@
+import discord
+import random
+import json
+from discord.ext import commands
+from core.classes import Cog_Extension
+
+with open('setting.json', mode='r',encoding='utf-8') as jFile: 
+    jdata = json.load(jFile)
+
+
+class React(Cog_Extension):
+    @commands.command()
+    # 本機圖片庫
+    async def weather(self,ctx):
+        pic = discord.File(jdata['pic'])
+        await ctx.send(file=pic)
+
+    @commands.command()
+    #網頁瀏覽器圖片
+    async def web(self,ctx):
+        pic = random.choice(jdata['url_pic'])
+        await ctx.send(pic)
+
+def setup(bot):
+    bot.add_cog(React(bot))
