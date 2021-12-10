6e6420483589d780f9d7a8218111c9e40435e579
b0989596914 <92772248+b0989596914@users.noreply.github.com>
2021-12-10 22:08:16 +0800

    Create main.py
    add main py

---
+++ cmds/main.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from core.classes import Cog_Extension
+
+
+class Main(Cog_Extension):
+    
+    @commands.command()
+    async def ping(self,ctx):
+        await ctx.send(f'{round(self.bot.latency*1000,2)} (ms)')
+
+    @commands.command()
+    async def hi(self,ctx):
+        await ctx.send('ABCD')
+
+# 註冊code
+def setup(bot):
+    bot.add_cog(Main(bot))
