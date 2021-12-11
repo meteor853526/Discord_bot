978df9335b06c24013c19b3ece7e4c898101f48e
Tina <b0989596914@gmail.com>
2021-12-11 18:32:02 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from core.classes import Cog_Extension
-
+import datetime
 
 class Main(Cog_Extension):
     
@@ -15,12 +15,21 @@ class Main(Cog_Extension):
 
     @commands.command()
     async def em(self,ctx):
-        embed=discord.Embed(title="天氣預報", description="關於今天", color=0x47e1ba)
+        embed=discord.Embed(title="天氣預報", description="關於今天", color=0x47e1ba,timestamp=datetime.datetime.now())
         embed.set_author(name="中央氣象局", url="https://www.cwb.gov.tw/V8/C/W/week.html")
         embed.add_field(name="早上", value="18 - 27", inline=True)
         embed.add_field(name="晚上", value="15 - 19", inline=True)
-        embed.set_footer(text="footer name")
+        # embed.set_footer(text="footer name")
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def sayd(self,ctx,*,msg):
+        await ctx.message.delete()
+        await ctx.send(msg)
+
+    @commands.command()
+    async def clean(self,ctx,num:int):
+        await ctx.channel.purge(limit=num+1)
 
 # 註冊code
 def setup(bot):
