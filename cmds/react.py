c6c1a958375ecb73096ba44c48dfb6d7cd2bb3d8
Tina <b0989596914@gmail.com>
2021-12-14 21:23:12 +0800

    Update react.py
    earthquake

--- cmds/react.py
+++ cmds/react.py
@@ -4,6 +4,7 @@ import json
 import sys
 sys.path.append('./')
 import Crawler
+import earthq
 
 from discord.ext import commands
 from core.classes import Cog_Extension
@@ -24,11 +25,16 @@ class React(Cog_Extension):
     async def web(self,ctx):
         pic = random.choice(jdata['url_pic'])
         await ctx.send(pic)
-    @commands.command()
 
+    @commands.command()
     #爬蟲 - 氣象圖片
     async def craw(self,ctx):
         await ctx.send(Crawler.crawler())
+
+    @commands.command()
+    #爬蟲 - 地震表
+    async def earthq(self,ctx):
+        await ctx.send(earthq.earth())
 
 def setup(bot):
     bot.add_cog(React(bot))
