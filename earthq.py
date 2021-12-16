8052ce3358014175b8e2eca5711ab679ed7e88e6
@fcu-d0989427 <75241765+meteor853526@users.noreply.github.com>
2021-12-16 20:30:13 +0800

    Update earthq.py

--- earthq.py
+++ earthq.py
@@ -2,8 +2,9 @@ import urllib.request as req
 import bs4
 import pandas as pd
 
-import pandas as pd
- 
- 
-bus_table = pd.read_html("https://www.ubus.com.tw/Booking/FareInquiry")
-print(bus_table)
+def earth():
+    df = pd.DataFrame()
+    for i in range(0,4):
+        url = f'https://scweb.cwb.gov.tw/zh-tw/earthquake/world/'
+        df = pd.concat([df, pd.read_html(url)[0]]) # 爬取+合併DataFrame
+    return df
