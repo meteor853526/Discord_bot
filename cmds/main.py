28f4fa41b0d26c22cbf02eb47aac981e9a5ad89e
Tina <b0989596914@gmail.com>
2021-12-17 23:44:52 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -10,26 +10,24 @@ class Main(Cog_Extension):
         await ctx.send(f'{round(self.bot.latency*1000,2)} (ms)')
 
     @commands.command()
-    async def hi(self,ctx):
-        await ctx.send('ABCD')
-
-    @commands.command()
-    async def em(self,ctx):
+    async def how(self,ctx):
         embed=discord.Embed(title="天氣預報", description="關於今天", color=0x47e1ba,timestamp=datetime.datetime.now())
         embed.set_author(name="中央氣象局", url="https://www.cwb.gov.tw/V8/C/W/week.html")
         embed.add_field(name="早上", value="18 - 27", inline=True)
         embed.add_field(name="晚上", value="15 - 19", inline=True)
         # embed.set_footer(text="footer name")
+        await ctx.channel.purge(limit=1)
         await ctx.send(embed=embed)
+
 
     # @commands.command()  //複讀機
     # async def sayd(self,ctx,*,msg):
     #     await ctx.message.delete()
     #     await ctx.send(msg)
 
-    @commands.command()  
-    async def clean(self,ctx,num:int):
-        await ctx.channel.purge(limit=num+1)
+    # @commands.command()  
+    # async def clean(self,ctx,num:int):
+    #     await ctx.channel.purge(limit=1)
 
 # 註冊code
 def setup(bot):
