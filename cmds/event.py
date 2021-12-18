9260ca8811f5da0e52f99624357eb5115b75a490
imlililili <78844463+imlililili@users.noreply.github.com>
2021-12-18 14:51:12 +0800

    Update event.py

--- cmds/event.py
+++ cmds/event.py
@@ -2,6 +2,7 @@ import discord
 import json
 from discord.ext import commands
 from core.classes import Cog_Extension
+import weather
 
 with open('setting.json', mode='r',encoding='utf-8') as jFile: 
     jdata = json.load(jFile)
@@ -13,7 +14,10 @@ class Event(Cog_Extension):
         '苗栗縣','台中市','彰化縣','南投縣','雲林縣','嘉義市','嘉義縣','台南市',
         '高雄市','屏東縣','台東縣','花蓮縣','宜蘭縣','澎湖縣','金門縣','連江縣']
         if (msg.content in keyword) and (msg.author != self.bot.user):
-            await msg.channel.send(msg.content)
+            embed=discord.Embed(title=msg.content, description=weather.today(msg.content))
+            embed.set_author(name='今天天氣')
+            await msg.channel.send(embed=embed)
+            # await msg.channel.send(msg.content)
 
 def setup(bot):
     bot.add_cog(Event(bot))
