4efa11f8caf0d469a018276ae5f7662e4510eb8c
imlililili <78844463+imlililili@users.noreply.github.com>
2021-12-18 14:56:19 +0800

    Update event.py
    台改臺

--- cmds/event.py
+++ cmds/event.py
@@ -10,8 +10,8 @@ with open('setting.json', mode='r',encoding='utf-8') as jFile:
 class Event(Cog_Extension):
     @commands.Cog.listener()
     async def on_message(self,msg):
-        keyword = ['基隆市','台北市','新北市','桃園縣','新竹市','新竹縣',
-        '苗栗縣','台中市','彰化縣','南投縣','雲林縣','嘉義市','嘉義縣','台南市',
+        keyword = ['基隆市','臺北市','新北市','桃園縣','新竹市','新竹縣',
+        '苗栗縣','臺中市','彰化縣','南投縣','雲林縣','嘉義市','嘉義縣','台南市',
         '高雄市','屏東縣','台東縣','花蓮縣','宜蘭縣','澎湖縣','金門縣','連江縣']
         if (msg.content in keyword) and (msg.author != self.bot.user):
             embed=discord.Embed(title=msg.content, description=weather.today(msg.content))
