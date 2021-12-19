0f4f173bed47ad71168de2aafa95e161ebcafcd3
imlililili <78844463+imlililili@users.noreply.github.com>
2021-12-19 23:42:53 +0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -24,36 +24,57 @@ def today(city_name):
         # 得到id = 'Cxxxxx'->去找後面的headers
         city_id[th.span.text] = th.get('id')
 
-    # city_name = '臺中市'
     for tr in table:
         th = tr.th
         if(th.span.text==city_name):
             td = tr.find('td',{'headers':'day1'})
             sp = td.find('span',{'class':'tem-C'})
-            # print(sp.text)
-    #     #取得下個標籤內的文字
-    #     name = td.text
-    #     print(name)
-        # #把字串中測站名稱去除，留下觀測時間
-        # sp = td.text.split()
-        # print(sp)
-        # #date = 01/1907:00
-        # date = sp[1]
-        
-        # #date = 201901/1907:00
-        # date = year + date
-
-        # #字串轉為datetime格式
-        # date = datetime.datetime.strptime(date, '%Y%m/%d%H:%M')
-
-        # #測站異常時，溫度='-'
-        # if not th.nextSibling.text == '-':
-        #     temp = float(th.nextSibling.text)
-        # else:
-        #     temp = -99
-
-        # print(name, temp, date)
-
-    #關閉模擬瀏覽器       
     driver.quit()
     return sp.text
+
+def weekly(city_name):
+    url = 'https://www.cwb.gov.tw/V8/C/W/week.html'
+
+    #啟動模擬瀏覽器
+    driver = webdriver.Chrome(r'C:\\Users\\liyin\Downloads\\chromedriver_win32\\chromedriver.exe')
+
+    #取得網頁代碼
+    driver.get(url)
+
+    #指定 lxml 作為解析器
+    soup = BeautifulSoup(driver.page_source, features='lxml')
+
+    #<table id='table1'>
+    table = soup.find('table',{'id':'table1'})
+    tbody = soup.find_all('tbody')
+    day = ['1','2','3','4','5','6','7']
+    # 儲存縣市的代碼以及名字
+    city_id = {}
+    for tr in tbody:
+        th = tr.th
+        # 得到id = 'Cxxxxx'->去找後面的headers
+        city_id[th.span.text] = th.get('id')
+    sp = []
+    # date存從接收指令那天開始的一周
+    date = []
+    # data：將星期與天氣做成物件
+    data = {}
+    for tr in tbody:
+        th = tr.th
+        
+        if(th.span.text==city_name):
+            for i in day:
+                # 找星期
+                d = table.find('th',{'id':'day'+i})
+                # 把日期跟\n去掉
+                t = str(d.text)[-4]+str(d.text)[-3]+str(d.text)[-2]
+                # 找溫度
+                td = tr.find('td',{'headers':'day'+i})
+                p = td.p
+                temp = p.find('span',{'class':'tem-C'})
+                # 把\u2002去掉
+                tt = str(temp.text)[0]+str(temp.text)[1]+' - '+str(temp.text)[-2]+str(temp.text)[-1]
+                sp.append(tt)
+                data[t] = tt
+    driver.quit()
+    return data
