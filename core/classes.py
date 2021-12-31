66c5c51ec1499bceda4f87dcfba9d681cb14fe12
imlililili <78844463+imlililili@users.noreply.github.com>
2021-12-31 15:24:49 +0800

    Update classes.py

--- core/classes.py
+++ core/classes.py
@@ -1,8 +1,13 @@
 from time import asctime
 import discord
 from discord.ext import commands
+from discord_components import *
 import json, asyncio, datetime
 import weather
+
+with open('setting.json', mode='r',encoding='utf-8') as jFile: 
+    jdata = json.load(jFile)
+
 # 同樣繼承bot的屬性
 class Cog_Extension(commands.Cog):
     def __init__(self,bot):
@@ -10,14 +15,18 @@ class Cog_Extension(commands.Cog):
         self.counter = 0
         async def time_task():
             await self.bot.wait_until_ready()
-            self.channel = self.bot.get_channel(918520656478486551)
+            self.channel = self.bot.get_channel(jdata['weather'])
             while not self.bot.is_closed():
                 now_time = datetime.datetime.now().strftime("%H%M")
-                with open('setting.json','r',encoding = 'utf-8')as jFile:
-                    jdata = json.load(jFile)
-                # week = weather.weekly(jdata["location"])
+                city = jdata['location']
                 if(now_time==jdata["time"] and self.counter == 0):
-                    await self.channel.send('im working!')
+                    print('im in this it success')
+                    # week = {}
+                    # week = weather.weekly(city)
+                    # embed=discord.Embed(title="未來一周天氣(白天)", url="https://www.cwb.gov.tw/V8/C/W/week.html", description=city, color=0x5592d3)
+                    
+                    # embed.add_field(name= week['day'][0], value=week[1], inline=True)
+                    await self.channel.send('im working')
                     self.counter = 1
                     await asyncio.sleep(1)
                 # else if :印出一次後 等到下一分鐘 記數歸零才能確保24hr後可以執行
